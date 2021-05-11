3112100bdfc395f733744cae92e8e906a577d5f6
hagoodwin <hagoodwi@gmail.com>
2021-05-11 15:14:36 -0700

    pass api call test 1

--- spec/covid_spec.rb
+++ spec/covid_spec.rb
@@ -2,9 +2,10 @@ require 'rspec'
 require 'covid'
 
 describe '#Covid' do
-  it('returns an api call') do
-    covid = Covid.new(' ')
-    expect(covid).to(eq(true))
+  it('returns an api call for a state') do
+    @covid = Covid.new(' ')
+    @al = @covid.al
+    expect(@al).to(eq(-1))
   end
 end
 
